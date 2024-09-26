6ca4ffc0ba87a553db4c8673453a0a6a96fcd912
tara-pogancev <tarapogancev@gmail.com>
2024-09-26 22:13:42 +0200

    Animation tree for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,29 @@
 extends CharacterBody2D
 
-var speed = 4
+const MAX_SPEED = 150
+const ACCELERATION = 500
+const FRICTION = 1000
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(delta: float):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO
+	input_vector = input_vector.normalized()
 	
-	move_and_collide(velocity)
+	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		
+	else:
+		#velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animation_state.travel("Idle")
+		velocity = Vector2.ZERO 
+	
+	move_and_slide()
 	
